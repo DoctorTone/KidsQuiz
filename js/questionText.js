f5e4f227c783ce0090a1520f81db66c2d1f98e2e
DoctorTone <drtonesoftware@gmail.com>
2017-09-07 01:08:14 +0100

    Read questions from file + progress timer

---
+++ js/questionText.js
@@ -0,0 +1,18 @@
+/**
+ * Created by DrTone on 07/09/2017.
+ */
+
+//Questions for childrens quiz
+
+let questionText = [
+    {
+        question: "What is the capital of England ?",
+        answers: ["London", "Paris", "New York"],
+        answer: 0
+    },
+    {
+        question: "What is the closest planet to the Sun?",
+        answers: ["Earth", "Saturn", "Mercury"],
+        answer: 2
+    }
+];
--- js/questions.js
+++ js/questions.js
@@ -2,7 +2,47 @@
  * Created by DrTone on 11/04/2017.
  */
 
+class QuestionManager {
+    constructor() {
+        this.currentQuestion = 0;
+        this.numAnswers = 3;
+        this.updateTime = 100;
+        this.currentTime = 0;
+        this.totalTime = 30 * 1000;
+    }
+
+    getUpdateTime() {
+        return this.updateTime;
+    }
+
+    updateTimer() {
+        this.currentTime += this.updateTime;
+        let progress = (this.currentTime / this.totalTime) * 100;
+        $('#progressBar').width(progress + "%");
+        if(this.currentTime >= this.totalTime) {
+            console.log("Timeout!");
+        }
+    }
+
+    setupNextQuestion() {
+        //Set up question
+        let question = questionText[this.currentQuestion];
+        let qNumber = this.currentQuestion + 1;
+        $('#questionNumber').html(qNumber);
+        $('#questionText').html(question.question);
+
+        //Set up answers
+        let baseText = "answer";
+        for(let i=0; i<this.numAnswers; ++i) {
+            $('#' + baseText + i).html(question.answers[i]);
+        }
+    }
+}
 
 $(document).ready( ()=> {
-
+    let qManager = new QuestionManager();
+    qManager.setupNextQuestion();
+    setInterval( () => {
+        qManager.updateTimer();
+    }, qManager.getUpdateTime());
 });
