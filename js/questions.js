b1a9da7cea668f4c5fdef193110c01f8a3d0b1ff
DoctorTone <drtonesoftware@gmail.com>
2017-09-07 17:19:44 +0100

    New button images, respponses to answers

--- js/questions.js
+++ js/questions.js
@@ -9,6 +9,8 @@ class QuestionManager {
         this.updateTime = 100;
         this.currentTime = 0;
         this.totalTime = 30 * 1000;
+        this.questionWaitTime = 3 * 1000;
+        this.timerRunning = true;
     }
 
     getUpdateTime() {
@@ -16,11 +18,27 @@ class QuestionManager {
     }
 
     updateTimer() {
+        if(!this.timerRunning) return;
+
         this.currentTime += this.updateTime;
         let progress = (this.currentTime / this.totalTime) * 100;
         $('#progressBar').width(progress + "%");
         if(this.currentTime >= this.totalTime) {
             console.log("Timeout!");
+        }
+    }
+
+    clearTimer() {
+        this.timerRunning = false;
+        this.currentTime = 0;
+        $('#progressBar').width("0%");
+    }
+
+    clearAnswers() {
+        for(let i=0; i<this.numAnswers; ++i) {
+            $('#answerCorrect' + i).hide();
+            $('#answerWrong' + i).hide();
+            $('#clickResponse' + i).hide();
         }
     }
 
@@ -32,9 +50,39 @@ class QuestionManager {
         $('#questionText').html(question.question);
 
         //Set up answers
-        let baseText = "answer";
+        let baseText = "answerText";
         for(let i=0; i<this.numAnswers; ++i) {
             $('#' + baseText + i).html(question.answers[i]);
+        }
+    }
+
+    nextQuestion() {
+        ++this.currentQuestion;
+        this.timerRunning = true;
+        this.clearAnswers();
+        this.setupNextQuestion();
+    }
+
+    showCorrectResponse(answer) {
+        let elem = $('#clickResponse' + answer);
+        elem.attr("src", "images/greatGreenButton.png");
+        elem.show();
+        this.clearTimer();
+        setTimeout( () => {
+            this.nextQuestion();
+        }, this.questionWaitTime);
+    }
+
+    checkAnswer(answerID) {
+        //Get answer number
+        let answer = answerID.match(/\d/g);
+        answer = answer.join("");
+        answer = parseInt(answer, 10);
+        let correctAnswer = questionText[this.currentQuestion].answer;
+        let correct = answer === correctAnswer;
+        correct ? $('#answerCorrect' + answer).show() : $('#answerWrong' + answer).show();
+        if(correct) {
+            this.showCorrectResponse(answer);
         }
     }
 }
@@ -45,4 +93,8 @@ $(document).ready( ()=> {
     setInterval( () => {
         qManager.updateTimer();
     }, qManager.getUpdateTime());
+
+    $('[id^="clickAnswer"]').on("click", function() {
+        qManager.checkAnswer(this.id);
+    });
 });
